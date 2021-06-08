ccba7bbbcc014e098d7df4ddcd27de022b55db90
Jorazon <oskari.pahkala@gmail.com>
2021-06-08 17:13:15 +0300

    added documantation and cleaned export

--- jsonio.js
+++ jsonio.js
@@ -1,12 +1,21 @@
 const fs = require("fs");
 
+/**
+ * Reads a json file
+ * @param {*} path
+ * @returns
+ */
 function readjson(path) {
 	return JSON.parse(fs.readFileSync(path));
 }
 
+/**
+ * Writes a json file
+ * @param {*} data
+ * @param {string} path
+ */
 function writejson(data, path) {
 	fs.writeFileSync(path, JSON.stringify(data));
 }
 
-exports.writejson = writejson;
-exports.readjson = readjson;
+module.exports = { readjson, writejson };
