38f4c551ad6665724359e12a10434256269981fb
Jorazon <oskari.pahkala@gmail.com>
2021-08-30 10:53:26 +0300

    slash command support

--- commands/commands.js
+++ commands/commands.js
@@ -1,6 +1,24 @@
-const { ping } = require("../commands/ping");
-const { prefix } = require("../commands/prefix");
-const { help } = require("../commands/help");
-const { connect } = require("../games/connect/game");
+import ping from "../commands/ping";
+import prefix from "../commands/prefix";
+import connect from "../games/connect/game";
 
-module.exports = { ping, prefix, help, connect };
+const refresh = async () => {
+	const commands = [
+		{
+			name: "ping",
+			description: "Replies with Pong!",
+		},
+	];
+
+	try {
+		console.log("Started refreshing application (/) commands.");
+
+		await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
+
+		console.log("Successfully reloaded application (/) commands.");
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+module.exports = { ping, prefix, connect };
