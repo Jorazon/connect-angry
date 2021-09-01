802a6b3a8f5ac8a79a179ce7d230882285f674d1
Jorazon <oskari.pahkala@gmail.com>
2021-09-02 01:06:25 +0300

    SelectMenu

--- games/connect/game.js
+++ games/connect/game.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const { rand } = require("../../rand");
+const { writejson } = require("../../jsonio");
 
 const players = {
 	EMPTY: 0,
@@ -9,18 +10,18 @@ const players = {
 
 //0-10 and blank keycap emojis
 const numbers = [
-	"0\uFE0F\u20E3",
-	"1\uFE0F\u20E3",
-	"2\uFE0F\u20E3",
-	"3\uFE0F\u20E3",
-	"4\uFE0F\u20E3",
-	"5\uFE0F\u20E3",
-	"6\uFE0F\u20E3",
-	"7\uFE0F\u20E3",
-	"8\uFE0F\u20E3",
-	"9\uFE0F\u20E3",
-	"\uD83D\uDD1F",
-	"\uD83D\uDFE6",
+	{ emoji: "0\uFE0F\u20E3", name: "Zero" },
+	{ emoji: "1\uFE0F\u20E3", name: "One" },
+	{ emoji: "2\uFE0F\u20E3", name: "Two" },
+	{ emoji: "3\uFE0F\u20E3", name: "Three" },
+	{ emoji: "4\uFE0F\u20E3", name: "Four" },
+	{ emoji: "5\uFE0F\u20E3", name: "Five" },
+	{ emoji: "6\uFE0F\u20E3", name: "Six" },
+	{ emoji: "7\uFE0F\u20E3", name: "Seven" },
+	{ emoji: "8\uFE0F\u20E3", name: "Eight" },
+	{ emoji: "9\uFE0F\u20E3", name: "Nine" },
+	{ emoji: "\uD83D\uDD1F", name: "Ten" },
+	{ emoji: "\uD83D\uDFE6", name: "Blank" },
 ];
 
 //emojis for White Circle, Pouting Face and Angry Face
@@ -34,43 +35,59 @@ const template = {
 };
 
 /**
+ * Generates board string
+ * @returns {string} game board string form
+ */
+function boardToString() {
+	let gameBoard = "";
+	for (let y = 0; y < 7; ++y) {
+		for (let x = 0; x < 7; ++x) {
+			if (y == 6) gameBoard += numbers[x + 1].emoji;
+			else gameBoard += emojis[rand(emojis.length)];
+		}
+		if (y != 6) gameBoard += "\n";
+	}
+	gameBoard += `\t${emojis[rand(emojis.length)]}'s turn`;
+	return gameBoard;
+}
+
+/**
  *
  * @param {Discord.Interaction} interaction
  */
 function connect(interaction) {
-	let gameBoard = "";
-	for (let y = 0; y < 7; ++y) {
-		for (let x = 0; x < 7; ++x) {
-			if (y == 6) gameBoard += numbers[x + 1];
-			else gameBoard += emojis[rand(emojis.length)];
-		}
-		gameBoard += "\n";
-	}
+	let gameBoard = boardToString();
 
 	var options = numbers
 		.slice()
 		.filter((v, i) => i >= 1 && i <= 7)
-		.map((emoji, i) => ({
-			label: `${i + 1}`,
+		.map((number, i) => ({
+			label: `${number.name}`,
 			value: `${i}_${interaction.id}`,
 			emoji: {
-				name: emoji,
+				name: number.emoji,
 				id: null,
 			},
 		}));
 
 	var reply = {
-		content: `${emojis[rand(emojis.length)]}'s turn`,
-		embeds: [new Discord.MessageEmbed({ title: gameBoard })],
+		content: gameBoard,
 		components: [
-			new Discord.MessageSelectMenu({
-				minValues: 1,
-				maxValues: 1,
-				options: options,
-			}).toJSON(),
+			{
+				type: 1,
+				components: [
+					new Discord.MessageSelectMenu({
+						custom_id: "col_select",
+						placeholder: "Choose column",
+						minValues: 1,
+						maxValues: 1,
+						options: options,
+					}).toJSON(),
+				],
+			},
 		],
 	};
-	console.log(reply);
+
 	interaction.reply(reply).catch((error) => {
 		console.error(error);
 	});
